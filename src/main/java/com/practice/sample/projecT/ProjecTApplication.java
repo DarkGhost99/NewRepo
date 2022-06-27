f50bb5ddc818437391cf2f39d24999a676086971
DarkGhost99 <pikuu9876@gmail.com>
2022-06-27 20:09:03 +0530

    sOME THING

---
+++ src/main/java/com/practice/sample/projecT/ProjecTApplication.java
@@ -0,0 +1,13 @@
+package com.practice.sample.projecT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/sample/projecT/ProjecTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.sample.projecT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
